c12f9719c1df4a06889a9695808e408017cdeb57
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-05-01 18:35:45 -0400

    formvalidation

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'be9f24f348942bc26cd365c2fc86b769'
@@ -17,13 +17,17 @@ posts = [
     }
 ]
 @app.route("/")
-def hello():
+def home():
     return render_template("home.html", posts=posts)
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 

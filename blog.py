94a289e1c71a17189eba6195ccb6114606fc9e7d
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-05-01 12:14:42 -0400

    added layout

--- blog.py
+++ blog.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
+posts = [
+    {
+        'author': 'Niraj Chowdhary',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Nikhil Chowdhary',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2020'
+    }
+]
 @app.route("/")
 def hello():
-    return "hello"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")

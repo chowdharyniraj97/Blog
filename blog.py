2571a912fa727bcd713b5c74b173e7114df77d84
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-05-01 20:18:07 -0400

    login page validation

--- blog.py
+++ blog.py
@@ -31,9 +31,16 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'chowdharyniraj97@gmail.com' and form.password.data == 'password':
+            flash('Welcome Niraj', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash("login unsuccessful, please check username and password!", 'danger')
+
     return render_template('login.html', title='Login', form=form)
 
 

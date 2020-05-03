669bffd6efcaf0ec2fa925bcf80ef4af37a86653
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-05-03 13:57:47 -0400

    added routes

--- BLog/route.py
+++ BLog/route.py
@@ -28,6 +28,7 @@ def register():
     if form.validate_on_submit():
         hashed_pw=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user=User(username=form.username.data,email=form.email.data,password=hashed_pw)
+        
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}! You can login now', 'success')

1808b12a3b775481e6bf9c77886d948c186bf04d
chwdharyniraj97 <chowdharyniraj97@gmail.com>
2020-05-06 18:55:35 -0400

    CRUD operations finished

--- BLog/route.py
+++ BLog/route.py
@@ -1,7 +1,7 @@
 import os,secrets
 from PIL import Image
 from BLog.models import User, Post
-from flask import  render_template, url_for, flash, redirect, request
+from flask import  render_template, url_for, flash, redirect, request,abort
 from BLog.forms import RegistrationForm, LoginForm, UpdateForm,PostForm
 from BLog import app,bcrypt,db
 from flask_login import login_user,current_user,logout_user,login_required
@@ -100,5 +100,41 @@ def new_posts():
         db.session.commit()
         flash('Your post has been created','success')
         return redirect(url_for("home"))
-    return render_template('creat_post.html', title='New Post',form=form)
+    return render_template('creat_post.html', title='New Post',form=form,legend="New Post")
 
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post=Post.query.get_or_404(post_id)
+    return render_template('post.html',title=post.title,post=post)
+
+@app.route("/post/<int:post_id>/update",methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post=Post.query.get_or_404(post_id)
+    if post.author!=current_user:
+        abort(403)
+    form=PostForm()
+    if form.validate_on_submit():
+        post.title=form.title.data
+        post.content=form.content.data
+        db.session.commit()
+        flash("Your post has been updated")
+        return redirect(url_for('post',post_id=post.id))
+
+    elif request.method=='GET':
+        form.title.data=post.title
+        form.content.data=post.content
+
+    form.title.data=post.title
+    form.content.data=post.content
+    return render_template('creat_post.html', title='Update Post',form=form,legend="Update Post")
+
+@app.route("/post/<int:post_id>/delete",methods=['POST'])
+def delete_post(post_id):
+    post=Post.query.get_or_404(post_id)
+    if post.author!=current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted",'success')
+    return redirect(url_for('home'))

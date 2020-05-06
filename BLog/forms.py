b92b3afd482aa52a3522e5f13c65a6a8e1fc1c33
chwdharyniraj97 <chowdharyniraj97@gmail.com>
2020-05-06 13:32:34 -0400

    added profile picture upload logic

--- BLog/forms.py
+++ BLog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
@@ -38,9 +39,10 @@ class LoginForm(FlaskForm):
     
     
 class UpdateForm(FlaskForm):
-    username = StringField('Username', validators=[
-                           DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username')
+    email = StringField('Email')
+
+    picture=FileField("Upadtae profile picture",validators=[FileAllowed(['jpg','png'])])
     
     submit = SubmitField('Update')
     
--- BLog/route.py
+++ BLog/route.py
@@ -1,3 +1,4 @@
+import os,secrets
 from BLog.models import User, Post
 from flask import  render_template, url_for, flash, redirect, request
 from BLog.forms import RegistrationForm, LoginForm, UpdateForm
@@ -60,6 +61,13 @@ def login():
 
     return render_template('login.html', title='Login', form=form)
 
+def save_pic(form_pic):
+    random_hex=secrets.token_hex(8)
+    _,f_ext=os.path.splitext(form_pic.filename)
+    picture_fn=random_hex+f_ext
+    pict_path=os.path.join(app.root_path,'static/profile_pic',picture_fn)
+    form_pic.save(pict_path)
+    return picture_fn
 
 @app.route("/about")
 def about():
@@ -75,10 +83,15 @@ def logout():
 def account():
     form=UpdateForm()
     if form.validate_on_submit():
-       current_user.username=form.username.data
-       current_user.email=form.email.data  
-       db.session.commit()
-       flash('Your accout has been updated successfully','success')   
-       return redirect(url_for('account'))
+        if form.picture.data:
+            picture_file=save_pic(form.picture.data)
+            current_user.image_file=picture_file
+
+
+        current_user.username=form.username.data
+        current_user.email=form.email.data  
+        db.session.commit()
+        flash('Your accout has been updated successfully','success')   
+        return redirect(url_for('account'))
     image_file=url_for('static',filename='profile_pic/'+current_user.image_file)
     return render_template('account.html', title='Account',image_file=image_file,form=form)

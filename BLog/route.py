8808235baf51ad32905d6f808184664b7bc15a7f
chwdharyniraj97 <chowdharyniraj97@gmail.com>
2020-05-07 11:48:24 -0400

    oarganized posts per page

--- BLog/route.py
+++ BLog/route.py
@@ -9,7 +9,8 @@ from flask_login import login_user,current_user,logout_user,login_required
 @app.route("/")
 @app.route("/index")
 def home():
-    posts=Post.query.all()
+    pag=request.args.get('page',1,type=int)
+    posts=Post.query.paginate(page=page,per_page=5)
     return render_template("home.html", posts=posts)
 
 

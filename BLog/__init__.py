de2a15f415229c55b06f2be6e583fea268f0d789
chwdharyniraj97 <chowdharyniraj97@gmail.com>
2020-05-08 11:47:04 -0400

    working reset link setup

--- BLog/__init__.py
+++ BLog/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from BLog import myEnvVal
+myEnvVal.setVar()
 
 
 

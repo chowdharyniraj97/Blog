899e5200436cc9bbc52bd75904b31122781d6238
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2020-05-22 15:56:44 -0400

    added wild card matching

--- app-routing/src/app/app-routing.module.ts
+++ app-routing/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DepartmentListComponent} from './department-list/department-list.component'
 import {EmployeeListComponent} from './employee-list/employee-list.component'
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component'
 
 
 const routes: Routes = [
+  {path:'', redirectTo:'/department', pathMatch:'full'},
   {path:'department',  component:DepartmentListComponent},
-  {path:'employee', component: EmployeeListComponent}
+  {path:'employee', component: EmployeeListComponent},
+  {path:"**", component:PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -14,4 +17,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routing=[DepartmentListComponent,EmployeeListComponent]
+export const routing=[DepartmentListComponent,EmployeeListComponent,PageNotFoundComponent]
--- app-routing/src/app/app.module.ts
+++ app-routing/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routing } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routing
+    routing,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,

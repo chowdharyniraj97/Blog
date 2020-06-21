29bc7502dc2f3157a7d21e52302926715ea1fbf0
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2020-06-21 13:41:25 -0400

    Added testing functions

--- BLog/Tests/test_demo.py
+++ BLog/Tests/test_demo.py
@@ -1,15 +1,48 @@
-import pytest
 import requests
 
 
 
 
 
-url='http://localhost:5000/'
+url='http://flask-sample:5000/'
 
-def test_testing():
+def test_home():
     data=requests.get(url)
     print(data)
     assert data.status_code==200
+
+def test_login():
+    data={
+        'email':'chowdharyniraj97@gmail.com',
+        'password':'12345678'
+    }
+    response=requests.post(url+'login',json=data)
+    print(response)
+    assert response.status_code==200
+
+def test_failed_login():
+    data={
+        'email':'chowdharyniraj97@gmail.com',
+        'password':'123456789075'
+    }
+    response=requests.post(url+'login',json=data)
+    assert response.status_code==404
+
+def test_reset_password():
+    data={
+        'email':'chowdharyniraj97@gmail.com' #add your email id if you want to test
+    }
+    response=requests.post(url+'reset_password',json=data)
+    assert response.status_code==200
+
+def test_add_post():
+    data={
+        'email':'chowdharyniraj97@gmail.com',
+        'title':'Testing',
+        'content':'sending content from pytest'
+    }
+    response=requests.post(url+'post/new',json=data)
+    assert response.status_code==200
+
 
 
